e7963eb9930310de18e427d925f2d4659f0ec406
maruthikoppu <student@pyb-7486-bchd.localdomain>
2020-07-14 10:51:37 -0400

    learning about directories

---
+++ list_challenge.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Create a list of cars with at least 5 eliments in it
+
+cars_list = ["car1", "car2", "car3", "car4", "car5" ]
+
+# Create a list of names with at least 5 eliments in it
+
+names_list = ["name1", "name2", "name3", "name4", "name5" ]
+
+# print out items from the third index of each list
+
+print(cars_list = (3))
+
+# print out the slice containing items from cars at indices 2-4 (inclusive)
+
+
+
+# print out the slice containing items from names at indices 0-2 (non-inclusive)
+
+
+
+# create a blank list named combined
+
+
+
+# Use the append method to add the cars into the list
+
+
+
+# Use the extend method to add the names into the list
+
+
+
+# print out 2 cars and 2 names from combined list
+
+
+
